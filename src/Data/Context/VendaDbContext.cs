d1b1a2fbc07991853aedee5e1dc13154a5dfbfea
Marcelo Santana <ext.Marcelo.Santana@vocedm.com.br>
2024-09-23 16:44:08 -0300

    feat: criação do VendaDbContext

---
+++ src/Data/Context/VendaDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+
+namespace Infrastructure.Data
+{
+    public class VendaDbContext : DbContext
+    {
+        public VendaDbContext(DbContextOptions<VendaDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Venda> Vendas { get; set; }
+        public DbSet<ItemVenda> ItensVenda { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
