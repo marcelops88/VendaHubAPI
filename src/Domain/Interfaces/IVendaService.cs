6d5c7a090525ca38b0d2023e295e1603336051d1
Marcelo Santana <ext.Marcelo.Santana@vocedm.com.br>
2024-09-23 20:26:16 -0300

    feat: criação da interface responsável por gerenciar as regras de negocio da venda.

---
+++ src/Domain/Interfaces/IVendaService.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IVendaService
+    {
+        Task<Venda> CreateVendaAsync(Venda venda);
+        Task<Venda> UpdateVendaAsync(Guid id, Venda venda);
+        Task DeleteVendaAsync(Guid id);
+        decimal CalcularValorTotal(Venda venda);
+        void CancelarItemVenda(ItemVenda item);
+    }
+}
