300f8465d869eecdacffa159cecd2b7ef291d987
Marcelo Santana <ext.Marcelo.Santana@vocedm.com.br>
2024-09-23 20:39:33 -0300

    feat: iniciando criação da service

---
+++ src/Domain/Services/VendaService.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+
+namespace Domain.Services
+{
+    public class VendaService : IVendaService
+    {
+        public decimal CalcularValorTotal(Venda venda)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CancelarItemVenda(ItemVenda item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Venda> CreateVendaAsync(Venda venda)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteVendaAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Venda>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Venda> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Venda> UpdateVendaAsync(Guid id, Venda venda)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
