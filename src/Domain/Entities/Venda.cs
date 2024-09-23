696859b2597cec79a05bc02d83e7d1b22c06980b
Marcelo Santana <ext.Marcelo.Santana@vocedm.com.br>
2024-09-23 18:10:07 -0300

    feat: adicionando novas propriedades as entidades

--- src/Domain/Entities/Venda.cs
+++ src/Domain/Entities/Venda.cs
@@ -5,10 +5,13 @@
         public int NumeroVenda { get; private set; }
         public DateTime DataVenda { get; private set; }
         public string NomeCliente { get; private set; }
+        public string CpfCliente { get; private set; }
+        public string TelefoneCliente { get; private set; }
+        public string EmailCliente { get; private set; }
         public string Filial { get; private set; }
         public List<ItemVenda> Itens { get; private set; }
 
-        public Venda(int numeroVenda, string nomeCliente, string filial, List<ItemVenda> itens)
+        public Venda(int numeroVenda, string nomeCliente, string filial, List<ItemVenda> itens, string cpfCliente, string telefoneCliente, string emailCliente)
         {
             NumeroVenda = numeroVenda;
             NomeCliente = nomeCliente;
@@ -16,12 +19,11 @@
             Itens = itens ?? new List<ItemVenda>();
             DataVenda = DateTime.UtcNow;
             Ativo = true;
+            CpfCliente = cpfCliente;
+            TelefoneCliente = telefoneCliente;
+            EmailCliente = emailCliente;    
         }
 
-        public void Cancelar()
-        {
-            Ativo = false;
-        }
 
         public decimal ValorTotal()
         {
